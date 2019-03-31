2fb95ac7ca24d0d58463ee03bf038e940a11fd35
Chris Navarro <cjnavarroWPI@gmail.com>
2019-03-31 08:18:06 -0400

    add map routing (hardcoded)

--- src/MapTest.js
+++ src/MapTest.js
@@ -1,20 +1,24 @@
 import React from 'react'
 import { Map, Marker, Popup, TileLayer } from 'react-leaflet'
-const MapTest = () => (
-  <div>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.4.0/leaflet.css" />
-      <Map center={[51.505, -0.09]} zoom={14}>
+import MapRoute from './MapRoute'
+import 'leaflet/dist/leaflet.css';
+
+const MapTest = () => {
+  let map = React.createRef();
+  return (
+    <Map center={[51.505, -0.09]} zoom={14} ref={map}>
         <TileLayer
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
           />
-        <Marker position={[51.505, -0.09]}>
+        <Marker position={[42.505, -71]}>
           <Popup>
             A pretty CSS3 popup. <br/> Easily customizable.
           </Popup>
         </Marker>
-      </Map>
-    </div>
-);
+        <MapRoute color="#3498DB" from={[42.402571,-71.1181906]} to={[43.3920026,-71.9017829]} map={map}/>
+    </Map>
+)
+};
 
 export default MapTest;
