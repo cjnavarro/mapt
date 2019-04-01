3cf6002652068caa06b02b34426f11919d9e5cf9
Chris Navarro <cjnavarroWPI@gmail.com>
2019-04-01 19:23:58 -0400

    install material ui-packages + some action testing

--- src/components/common/Navigation.js
+++ src/components/common/Navigation.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 
 const Navigation = ({children}) =>
 (
-  <div className="navigation">
-    <nav>
-      <NavLink to="/home">
-        Home
-      </NavLink>
-      <NavLink to="/about">
-        About
-      </NavLink>
-    </nav>
+  <div>
+    <AppBar className="navigation" position="static">
+      <Toolbar>
+        <IconButton color="inherit" aria-label="Menu">
+          <MenuIcon />
+        </IconButton>
+        <NavLink to="/home">
+          Home
+        </NavLink>
+        <NavLink to="/about">
+          About
+        </NavLink>
+        <Typography variant="h6" color="inherit">
+                  News
+        </Typography>
+      </Toolbar>
+    </AppBar>
     {children}
   </div>
 );
--- src/components/common/RouteList.js
+++ src/components/common/RouteList.js
@@ -1,16 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 
 const RouteList = ({routes, actions}) =>
-(
-  <ul>
-    {routes.map(route =>
-      <div key={route.id}>
-        From: {route.from[0]}, {route.from[1]} To: {route.to[0]}, {route.to[1]}
-      </div>
-    )}
-  </ul>
-)
+{
+  const [coordinate, setCoordinate] = useState("");
+  return (
+    <ul>
+      {routes.map(route =>
+        <div key={route.id}>
+          From: {route.from[0]}, {route.from[1]} To: {route.to[0]}, {route.to[1]}
+        </div>
+      )}
+
+      <form onSubmit={() => actions.addRoute([coordinate, 0], [2,3])}>
+        <label>
+          Number:
+          <input type="number" onChange={(e) => setCoordinate(e.target.value)}/>
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+
+      <button onClick={() => actions.deleteRoute(0)}>Delete ME</button>
+    </ul>
+  )
+}
 
 RouteList.propTypes = {
   routes: PropTypes.array.isRequired,
