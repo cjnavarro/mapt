3a46d16022eda9b9f30953fdb3446348e47b58be
Chris Navarro <cjnavarroWPI@gmail.com>
2019-04-03 07:36:35 -0400

    working form

--- src/components/common/GearLoadout.js
+++ src/components/common/GearLoadout.js
@@ -1,43 +1,42 @@
-import React, { useRef } from 'react'
+import React, { useState, useRef } from 'react'
 import PropTypes from 'prop-types'
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 
 const GearLoadout = ({gear, actions}) =>
 {
-  let year = useRef(gear.bike.year);
-  let make = useRef(gear.bike.make);
-  let model = useRef(gear.bike.model);
+  const [year, setYear] = useState(gear.bike.year);
+  const [make, setMake] = useState(gear.bike.make);
+  const [model, setModel] = useState(gear.bike.model);
 
   return (
     <div>
       <TextField
-        defaultValue={gear.bike.year}
-        ref={year}
-        onChange={(e) => year = e.target.value}
+        defaultValue={year}
+        onChange={(e) => setYear(e.target.value)}
         placeholder="Year"
         variant="outlined"
         label="Year"
         />
       <TextField
-        defaultValue={gear.bike.make}
-        ref={make}
-        onChange={(e) => make = e.target.make}
+        defaultValue={make}
+        onChange={(e) => setMake(e.target.value)}
         placeholder="Make"
         variant="outlined"
         label="Make"
         />
       <TextField
-        defaultValue={gear.bike.model}
-        ref={model}
-        onChange={(e) => year = e.target.model}
+        defaultValue={model}
+        onChange={(e) => setModel(e.target.value)}
         placeholder="Model"
         variant="outlined"
         label="Model"
         />
-      <Button onClick={() => console.log(year)}>
+      <Button onClick={() => actions.editGear({year, make, model}, "Joe Rocket")}>
         Save
       </Button>
+
+      {gear.jacket}
     </div>
   )
 }
