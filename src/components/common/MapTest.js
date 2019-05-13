4f1cfae2fa452a0f5a7a478e5d7c3d1d9019a108
Chris Navarro <cjnavarroWPI@gmail.com>
2019-05-13 08:24:55 -0400

    add bounds and dark mode

--- src/components/common/MapTest.js
+++ src/components/common/MapTest.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import { Map, Marker, Popup, TileLayer } from 'react-leaflet'
+import { latLngBounds } from 'leaflet'
 import MapRoute from './MapRoute'
-import 'leaflet/dist/leaflet.css';
+import 'leaflet/dist/leaflet.css'
 
 const MapTest = () => {
   let map = React.createRef();
+  
   return (
-    <Map center={[51.505, -0.09]} zoom={14} ref={map}>
-        <TileLayer
-          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-          url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+    <Map center={[0, 0]} minZoom={2} maxZoom={7} maxBounds={latLngBounds([-90, -180], [90, 180])} zoom={2} ref={map}>
+        <TileLayer url='https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png'
           />
     </Map>
 )
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {render} from 'react-dom';
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
-import reducer from './reducers'
+import reducer from './reducers/index'
 
 import App from './components/App'
 import './index.css'
