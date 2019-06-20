723a100ba218fecb49a0a788ac8cb8a50bd47b5c
Chris Navarro <cjnavarroWPI@gmail.com>
2019-06-20 08:01:56 -0400

    working basic auth

--- src/api-handler.js
+++ src/api-handler.js
@@ -1,14 +1,22 @@
 import fetch from 'cross-fetch'
 
 export default ApiHandler => (url, opts) => {
-  // const headers = {
-  //   'Authorization': 'Token zV4nUhPvjZvpRPR4ZP98iNQzCTzaRkg43myt7UyzRCwsfc5UhLtRmIW6dxJ2ByGx'
-  // }
 
-  const combinedOptions = Object.assign({}, {}, opts)
+  //TODO right to localStorage
+
+  let headers = new Headers();
+
+  headers.append('Content-Type', 'text/json');
+  headers.append('Authorization', 'Basic Q2hyaXMgTmF2YXJybzpwYXNzd29yZA==');
+
+  const combinedOptions = Object.assign({}, headers, opts)
 
   return (
-    fetch('http://localhost:8080/api/' + url, combinedOptions)
+    fetch('http://localhost:8080/api/' + url,
+      { method:'GET',
+        //mode: 'no-cors',
+        headers: {Authorization: 'Basic Q2hyaXMgTmF2YXJybzpwYXNzd29yZA=='}
+      })
       // let's assume we're always getting JSON back
       .then(res => {
         // here we can check for whatever the API does
@@ -16,7 +24,6 @@ export default ApiHandler => (url, opts) => {
         console.log(res);
 
         if (res.status === 401) {
-          console.log('here');
           window.location.href = '/#/login';
           return {};
         }
