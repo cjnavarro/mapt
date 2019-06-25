3046bc3eb5bbb9affcbbc4fb3f5d87dc18f6a15b
Chris Navarro <cjnavarroWPI@gmail.com>
2019-06-24 20:43:20 -0400

    Update LoginContent.js

--- src/components/login/LoginContent.js
+++ src/components/login/LoginContent.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import  { Redirect, Link } from 'react-router-dom';
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
 
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
