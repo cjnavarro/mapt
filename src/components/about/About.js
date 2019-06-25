8ed6e90f35ec0d1d4fae293893ad02909a17d7ab
Chris Navarro <cjnavarroWPI@gmail.com>
2019-06-24 21:15:25 -0400

    Revert to card layout

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -2,10 +2,9 @@ import React from 'react';
 import Navigation from '../common/Navigation';
 
 import Typography from '@material-ui/core/Typography';
-//import Grid from '@material-ui/core/Grid';
+import Grid from '@material-ui/core/Grid';
 
-//import LogoCard from '../common/LogoCard';
-import Deck from '../common/Deck';
+import LogoCard from '../common/LogoCard';
 
 import reactLogo from '../common/images/react.png';
 import reduxLogo from '../common/images/redux.png';
@@ -14,11 +13,11 @@ import dropWizardLogo from '../common/images/dropwizard.png';
 import awsLogo from '../common/images/aws.png';
 
 const cards = [
-  { image: awsLogo, desc: 'EC2 instance (RHEL 7)' },
-  { image: dropWizardLogo, desc: 'Version 1.3.12' },
-  { image: mySqlLogo, desc: 'Version 8.0' },
-  { image: reactLogo, desc: 'Version 16.8.6' },
-  { image: reduxLogo, desc: 'Version 4.0.1' }
+  { title: 'AWS', image: awsLogo, desc: 'EC2 Instance', link: 'https://aws.amazon.com/' },
+  { title: 'Dropwizard', image: dropWizardLogo, desc: 'Version 1.3.12', link: 'https://www.dropwizard.io/1.3.12/docs/getting-started.html' },
+  { title: 'MySql', image: mySqlLogo, desc: 'Version 8.0', link: 'https://www.mysql.com/' },
+  { title: 'ReactJS', image: reactLogo, desc: 'Version 16.8.6', link: 'https://reactjs.org/' },
+  { title: 'Redux', image: reduxLogo, desc: 'Version 4.0.1', link: 'https://redux.js.org/' }
 ];
 
 const About = () => (
@@ -26,7 +25,14 @@ const About = () => (
     <Typography variant="h5" paragraph={true} align="center">
       Here are a few of the tools used to create this app.
     </Typography>
-    <Deck cards={cards}/>
+
+    <Grid container spacing={8}>
+      (cards.map(card) => {
+        <Grid item xs>
+          <LogoCard title={card.title} description={card.desc} image={card.image}
+            link={card.link}/>
+        </Grid>
+      });
   </Navigation>
 );
 
