e4f99f57b83a3e34998b9e050172fcc35a15ae66
Chris Navarro <cjnavarroWPI@gmail.com>
2019-06-26 07:25:13 -0400

    add get

--- src/containers/ResourcesContainer.js
+++ src/containers/ResourcesContainer.js
@@ -2,13 +2,15 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import ResourcesContent from '../components/resources/ResourcesContent';
 
+import { sendGet } from '../reducers/api';
+
 const mapStateToProps = state => ({
   token: state.api.token,
   loggedIn: state.api.loggedIn
 });
 
 const mapDispatchToProps = dispatch => ({
-  actions: bindActionCreators({}, dispatch)
+  actions: bindActionCreators({ sendGet }, dispatch)
 });
 
 const ResourcesContainer = connect(
