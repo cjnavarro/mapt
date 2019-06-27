a5ae218cf49e0a1297ba5bd33b52b498b22fc357
Chris Navarro <cjnavarroWPI@gmail.com>
2019-06-27 07:16:07 -0400

    remove popups (they're ugly)

--- src/components/map/MapContent.js
+++ src/components/map/MapContent.js
@@ -2,6 +2,8 @@ import React from 'react';
 import  { Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
+import Typography from '@material-ui/core/Typography';
+
 import { Map, Marker, TileLayer, LayerGroup, Popup } from 'react-leaflet';
 import MarkerClusterGroup from 'react-leaflet-markercluster';
 import L from 'leaflet';
@@ -41,6 +43,10 @@ const MapContent = ({actions, token, loggedIn}) => {
   });
 
   return (
+    <div>
+      <Typography variant="h5" paragraph={true}>
+        Leaflet marker cluster testing.
+      </Typography>
     <Map className="map" center={[42.505, -71]} minZoom={2} maxZoom={12} maxBounds={L.latLngBounds([-90, -180], [90, 180])} zoom={2}>
       <TileLayer url='https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png'/>
       <LayerGroup ref={(map) => {
@@ -50,26 +56,15 @@ const MapContent = ({actions, token, loggedIn}) => {
         }
         }}/>
       <MarkerClusterGroup>
-        <Marker position={[42.354940, -71.056590]}>
-          <Popup><div>iboss</div></Popup>
-        </Marker>
-        <Marker position={[42.273960, -71.809680]}>
-          <Popup><div>WPI</div></Popup>
-        </Marker>
-        <Marker position={[43.392070, -71.900200]}>
-          <Popup><div>Hometown</div></Popup>
-        </Marker>
-        <Marker position={[55.797200, 37.531920]}>
-          <Popup><div>WPI IQP</div></Popup>
-        </Marker>
-        <Marker position={[18.401430, -66.046710]}>
-          <Popup><div>Vacation</div></Popup>
-        </Marker>
-        <Marker position={[55.953251, -3.188267]}>
-          <Popup><div>Vacation</div></Popup>
-        </Marker>
+        <Marker position={[42.354940, -71.056590]} />
+        <Marker position={[42.273960, -71.809680]} />
+        <Marker position={[43.392070, -71.900200]} />
+        <Marker position={[55.797200, 37.531920]} />
+        <Marker position={[18.401430, -66.046710]} />
+        <Marker position={[55.953251, -3.188267]} />
       </MarkerClusterGroup>
     </Map>
+  </div>
 )};
 
 MapContent.propTypes = {
