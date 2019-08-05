2accff59f2c9aea1310fdccdd3524b0761984142
Chris Navarro <cjnavarroWPI@gmail.com>
2019-08-05 07:47:42 -0400

    test

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -1,9 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import { render, unmountComponentAtNode } from 'react-dom';
+import store from '../configureStore'
+import { Provider } from 'react-redux'
+
+import App from './App'
+import '../index.css'
+import * as serviceWorker from '../serviceWorker'
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  render(
+    <Provider store={store}>
+      <App />
+    </Provider>, div);
+  unmountComponentAtNode(div);
 });
